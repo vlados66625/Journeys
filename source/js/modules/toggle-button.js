60a8cd2b1d0226df9401288368baecff950a64f8
Владислав <vlados66625@mail.ru>
2023-09-08 19:31:32 +0300

    добавляет блок обучение

--- source/js/modules/toggle-button.js
+++ source/js/modules/toggle-button.js
@@ -1,20 +1,27 @@
 let navMain = document.querySelector('.header-top__navigation');
 let navToggle = document.querySelector('.header-top__toggle');
+let navLogo = document.querySelector('.header-top__logo');
+let navTelephoneIcon = document.querySelector('.header-top__telephone');
+
 
 export const addEvtToggleButton = () => {
-  if (navMain && navToggle) {
+  if (navMain && navToggle && navLogo && navTelephoneIcon) {
     navToggle.addEventListener('click', function () {
       if (navMain.classList.contains('header-top__navigation--closed')) {
         navMain.classList.remove('header-top__navigation--closed');
         navMain.classList.add('header-top__navigation--opened');
         navToggle.classList.remove('header-top__toggle--closed');
         navToggle.classList.add('header-top__toggle--opened');
+        navLogo.classList.add('header-top__logo-icon--dark');
+        navTelephoneIcon.classList.add('header-top__telephone--dark');
       } else {
         navMain.classList.add('header-top__navigation--closed');
         navMain.classList.remove('header-top__navigation--opened');
         navToggle.classList.add('header-top__toggle--closed');
         navToggle.classList.remove('header-top__toggle--opened');
+        navLogo.classList.remove('header-top__logo-icon--dark');
+        navTelephoneIcon.classList.remove('header-top__telephone--dark');
       }
     });
   }
-}
+};
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -5,9 +5,60 @@ import Swiper from './vendor/swiper';
 window.addEventListener('DOMContentLoaded', () => {
   const heroSwiper = new Swiper('.header-bottom', {
     loop: true,
+    speed: 300,
     pagination: {
       el: '.header-bottom__swiper-pagination',
       clickable: true,
     },
   });
+
+  const upcomingTours = new Swiper('.upcoming-tours__swiper', {
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.upcoming-tours__button-next',
+      prevEl: '.upcoming-tours__button-prev',
+    },
+  });
+
+  const trainingSwiper = new Swiper('.training__swiper', {
+    loop: false,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.training__button-next',
+      prevEl: '.training__button-prev',
+    },
+  });
+
+  const updateSwiperOptions = () => {
+    if (window.innerWidth >= 1200) {
+      upcomingTours.params.spaceBetween = 30;
+      upcomingTours.params.slidesPerView = 3;
+
+      trainingSwiper.params.slidesPerView = 4;
+    } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+      upcomingTours.params.slidesPerView = 2;
+      upcomingTours.params.spaceBetween = 18;
+
+
+      trainingSwiper.params.slidesPerView = 3;
+    } else {
+      upcomingTours.params.slidesPerView = 1;
+
+      trainingSwiper.params.slidesPerView = 1;
+    }
+    upcomingTours.update();
+    upcomingTours.updateSlides();
+
+
+    trainingSwiper.update();
+    trainingSwiper.updateSlides();
+  };
+
+
+  updateSwiperOptions();
+  window.addEventListener('resize', updateSwiperOptions);
+
 });
